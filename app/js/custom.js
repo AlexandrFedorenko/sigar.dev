733670f5899579f88529d1d663b2337824bcfdaf
AlexandrFedorenko <alexandrfedorenkoooo@gmail.com>
2018-01-08 19:00:23 +0300

    finish

--- app/js/custom.js
+++ app/js/custom.js
@@ -52,6 +52,95 @@ $(".bars-sidebar-js").click(function (e) {
     }
 });
 
+//filter slider
+$("#slider2").slider({
+    min: 0,
+    max: 10,
+    values: [2, 5],
+    range: true,
+    stop: function (event, ui) {
+        $("input#minSmol").val($("#slider2").slider("values", 0));
+        $("input#maxSmol").val($("#slider2").slider("values", 1));
+    },
+    slide: function (event, ui) {
+        $("input#minSmol").val($("#slider2").slider("values", 0));
+        $("input#maxSmol").val($("#slider2").slider("values", 1));
+    }
+});
+$("#slider").slider({
+    min: 0,
+    max: 1000,
+    values: [50, 500],
+    range: true,
+    stop: function (event, ui) {
+        $("input#minCost").val($("#slider").slider("values", 0));
+        $("input#maxCost").val($("#slider").slider("values", 1));
+    },
+    slide: function (event, ui) {
+        $("input#minCost").val($("#slider").slider("values", 0));
+        $("input#maxCost").val($("#slider").slider("values", 1));
+    }
+});
+
+//sliders
+
+$('.main-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    dots: true
+});
+
+$('.slider-popular-item').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    autoplay: false,
+    nextArrow: '.right',
+    prevArrow: '.left',
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }
+    ]
+});
+
+
+
+//mascedinput
+
+$(function($){
+    $("#phone").mask("999 999 9999",{placeholder:" "});
+    $("#phonePopup").mask("999 999 9999",{placeholder:" "});
+    $("#mainPhone").mask("999 999 9999",{placeholder:" "});
+});
+
+
+//popups
+
+$('.popup-with-form').magnificPopup({
+    type: 'inline',
+    focus: '#name'
+});
+
+
+
+
+
 
 
 
